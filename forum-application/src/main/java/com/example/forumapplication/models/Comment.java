4a1e9718395e1ee278e8a1cd2f72a4ee1bbc8b0d
AlexanderSlavchev <166736509+AlexanderSlavchev@users.noreply.github.com>
2024-07-20 18:11:21 +0300

    Imported Jakarta.validation in build.gradle
    Dtos Updated

--- forum-application/src/main/java/com/example/forumapplication/models/Comment.java
+++ forum-application/src/main/java/com/example/forumapplication/models/Comment.java
@@ -1,4 +1,19 @@
 package com.example.forumapplication.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "comments")
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String content;
+
 }
--- forum-application/src/main/java/com/example/forumapplication/models/dto/UserDto.java
+++ forum-application/src/main/java/com/example/forumapplication/models/dto/UserDto.java
@@ -1,4 +1,39 @@
 package com.example.forumapplication.models.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+
+@Getter
+@Setter
 public class UserDto {
+
+    @NotEmpty(message = "Username cannot be empty!")
+    @Size(min = 4, max = 32, message = "First name must be between 4 and 32 characters long!")
+    private String firstName;
+
+    @NotEmpty(message = "Last name cannot be empty!")
+    @Size(min = 4, max = 32, message = "Last name must be between 4 and 32 characters long!")
+    private String lastName;
+
+    @NotEmpty(message = "Email cannot be empty!")
+    @Email
+    private String email;
+
+    @NotEmpty
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?!.* ).{4,12}$",
+            message = "Password must contain one digit from 1 to 9, " +
+                    "one lowercase letter, " +
+                    "one uppercase letter, " +
+                    "one special character, " +
+                    "no space, and " +
+                    "it must be 4-12 characters long.")
+    private String password;
+
+
 }
