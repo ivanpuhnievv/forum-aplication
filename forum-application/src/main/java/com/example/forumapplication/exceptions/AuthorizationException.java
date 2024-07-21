6027199fad4efbaf79af52cc1aac2937cd862020
IvanPuhniev <ivan.puhniev.a60@learn.telerikacademy.com>
2024-07-21 19:10:28 +0300

    Added Exceptions

---
+++ forum-application/src/main/java/com/example/forumapplication/exceptions/AuthorizationException.java
@@ -0,0 +1,9 @@
+package com.example.forumapplication.exceptions;
+
+public class AuthorizationException extends RuntimeException {
+
+    public AuthorizationException(String message) {
+        super(message);
+    }
+
+}
---
+++ forum-application/src/main/java/com/example/forumapplication/exceptions/EntityDuplicateException.java
@@ -0,0 +1,9 @@
+package com.example.forumapplication.exceptions;
+
+public class EntityDuplicateException extends RuntimeException {
+
+    public EntityDuplicateException(String type, String attribute, String value) {
+        super(String.format("%s with %s %s already exists.", type, attribute, value));
+    }
+
+}
