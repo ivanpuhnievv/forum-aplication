db1f45d4d633b8ec2538b202afbd4773515dc692
IvanPuhniev <ivan.puhniev.a60@learn.telerikacademy.com>
2024-07-21 19:08:28 +0300

    Added join column in models/Post

--- forum-application/src/main/java/com/example/forumapplication/models/Post.java
+++ forum-application/src/main/java/com/example/forumapplication/models/Post.java
@@ -2,21 +2,30 @@ package com.example.forumapplication.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "createdBy_id")
     private User createdBy;
     private String title;
     private String content;
-
     @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Set<Comment> comments;
-
     @JsonIgnore
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
