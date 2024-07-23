5167f616277adbe2cf7a88c836757ce88b00db8e
AlexanderSlavchev <166736509+AlexanderSlavchev@users.noreply.github.com>
2024-07-23 18:29:30 +0300

    Updated User Functionality

--- forum-application/src/main/java/com/example/forumapplication/security/SecurityConfig.java
+++ forum-application/src/main/java/com/example/forumapplication/security/SecurityConfig.java
@@ -5,20 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    public static final int BCRYPT_STRENGTH = 11;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers(
-                                "api/test/**",
+                                "/api/test/**",
                                 "/swagger-ui/**",
                                 "/v2/api-docs",
                                 "/javainuse-openapi/**",
@@ -30,18 +35,31 @@ public class SecurityConfig {
                                 "/webjars/**",
                                 "configuration/ui",
                                 "configuration/security",
-                                "api/users/**"
+                                "/api/users/**", // Обновено: правилният път
+                                "/api/users"
                         )
                         .permitAll()
                         .anyRequest().authenticated()
                 )
-                .csrf(AbstractHttpConfigurer::disable);
+                .csrf(cr -> cr.disable())
+                .httpBasic(withDefaults());
         return http.build();
     }
 
-   @Bean
+    @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-   }
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
+
+    }
 
 }
