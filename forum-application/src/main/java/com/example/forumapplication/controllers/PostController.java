ead7b03de47a47d6bc64ad8b3a11b5aef89064a8
IvanPuhniev <ivan.puhniev.a60@learn.telerikacademy.com>
2024-07-24 11:23:54 +0300

    Added PostController and AuthenticationHelper

--- forum-application/src/main/java/com/example/forumapplication/controllers/PostController.java
+++ forum-application/src/main/java/com/example/forumapplication/controllers/PostController.java
@@ -1,11 +1,111 @@
 package com.example.forumapplication.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.forumapplication.exceptions.AuthorizationException;
+import com.example.forumapplication.exceptions.EntityDuplicateException;
+import com.example.forumapplication.exceptions.EntityNotFoundException;
+import com.example.forumapplication.helpers.AuthenticationHelper;
+import com.example.forumapplication.mappers.PostMapper;
+import com.example.forumapplication.models.Post;
+import com.example.forumapplication.models.User;
+import com.example.forumapplication.models.dtos.PostDto;
+import com.example.forumapplication.services.contracts.PostService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
 
+    private final PostService postService;
+    private final AuthenticationHelper authenticationHelper;
+    private final PostMapper mapper;
 
+    @Autowired
+    public PostController(PostService postService,PostMapper mapper,AuthenticationHelper authenticationHelper) {
+        this.postService = postService;
+        this.mapper = mapper;
+        this.authenticationHelper = authenticationHelper;
+    }
+
+    // Get all posts
+    @GetMapping
+    public List<Post> findAll(
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) User user){
+
+        return postService.get();
+    }
+
+    // Get post by ID
+    @GetMapping("/{id}")
+    public Post get(@PathVariable int id) {
+        try {
+            return postService.getById(id);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+    // Get post by title
+    @GetMapping("/title/{title}")
+    public Post getPostByTitle(@PathVariable String title) {
+        try {
+            return postService.getByTitle(title);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+    // Create a new post
+    @PostMapping
+    public Post create(@RequestHeader HttpHeaders headers, @Valid @RequestBody PostDto postDto) {
+        try {
+            User user = authenticationHelper.tryGetUser(headers);
+            Post post = mapper.fromDto(postDto);
+            postService.create(post, user);
+            return post;
+        }catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (EntityDuplicateException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        }
+    }
+
+    // Update an existing post
+    @PutMapping("/{id}")
+    public Post update(@RequestHeader HttpHeaders headers,@PathVariable int id, @Valid @RequestBody PostDto postDto) {
+        try {
+            User user = authenticationHelper.tryGetUser(headers);
+            Post post = mapper.fromDto(id, postDto);
+            postService.update(post, user);
+            return post;
+        }catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (EntityDuplicateException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        }
+    }
+
+    // Delete a post
+    @DeleteMapping("/{id}")
+    public void delete(@RequestHeader HttpHeaders headers, @PathVariable int id) {
+        try {
+            User user = authenticationHelper.tryGetUser(headers);
+            postService.delete(id, user);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        }
+    }
 }
