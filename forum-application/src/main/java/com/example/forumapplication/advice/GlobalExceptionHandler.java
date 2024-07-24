7af7302886f27b0def95c4b9c9080e76e806189d
AlexanderSlavchev <166736509+AlexanderSlavchev@users.noreply.github.com>
2024-07-24 12:23:37 +0300

    Added a GlobalExeption Handler

---
+++ forum-application/src/main/java/com/example/forumapplication/advice/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.example.forumapplication.advice;
+
+import com.example.forumapplication.exceptions.AuthorizationException;
+import com.example.forumapplication.exceptions.EntityDuplicateException;
+import com.example.forumapplication.exceptions.EntityNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleException(MethodArgumentNotValidException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(error -> {
+
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        });
+        return errorMap;
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    public Map<String, String> handleException(AuthorizationException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("error message ", exception.getMessage());
+        return errorMap;
+    }
+
+    @ExceptionHandler(EntityDuplicateException.class)
+    public Map<String, String> handleException(EntityDuplicateException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("error message ", exception.getMessage());
+        return errorMap;
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public Map<String, String> handleException(EntityNotFoundException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("error message ", exception.getMessage());
+        return errorMap;
+    }
+
+}
--- forum-application/src/main/java/com/example/forumapplication/services/UserServiceImpl.java
+++ forum-application/src/main/java/com/example/forumapplication/services/UserServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 import static com.example.forumapplication.filters.specifications.UserSpecifications.*;
 
