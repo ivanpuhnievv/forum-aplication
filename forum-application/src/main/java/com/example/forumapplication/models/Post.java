a0f8b209a3206a0f77b7acf61847784a3ff98457
IvanPuhniev <ivan.puhniev.a60@learn.telerikacademy.com>
2024-07-24 09:15:30 +0300

    added time stamps to post

--- forum-application/src/main/java/com/example/forumapplication/models/Post.java
+++ forum-application/src/main/java/com/example/forumapplication/models/Post.java
@@ -2,9 +2,11 @@ package com.example.forumapplication.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Data
@@ -20,8 +22,10 @@ public class Post {
     @JoinColumn(name = "createdBy_id")
     private User createdBy;
 
+    @NotBlank(message = "Title is mandatory")
     private String title;
 
+    @NotBlank(message = "Content is mandatory")
     private String content;
 
     @JsonIgnore
@@ -37,4 +41,29 @@ public class Post {
             inverseJoinColumns = @JoinColumn(name = "post_id")
     )
     private Set<User> likes;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @Column(nullable = false)
+    private LocalDateTime lastModifiedDate;
+
+//    @ManyToMany(fetch = FetchType.LAZY)
+//    @JoinTable(
+//            name = "post_tags",
+//            joinColumns = @JoinColumn(name = "post_id"),
+//            inverseJoinColumns = @JoinColumn(name = "tag_id")
+//    )
+//    private Set<Tag> tags;
+
+    @PrePersist
+    protected void onCreate() {
+        createdDate = LocalDateTime.now();
+        lastModifiedDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        lastModifiedDate = LocalDateTime.now();
+    }
 }
