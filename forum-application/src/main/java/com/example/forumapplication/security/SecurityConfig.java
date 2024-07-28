ecc6c048e2361dfbbec3dd74656a5b797550a60f
AlexanderSlavchev <166736509+AlexanderSlavchev@users.noreply.github.com>
2024-07-29 01:12:59 +0300

    LoginPage updated
    Basic Auth Working now

--- forum-application/src/main/java/com/example/forumapplication/security/SecurityConfig.java
+++ forum-application/src/main/java/com/example/forumapplication/security/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.example.forumapplication.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,6 +24,7 @@ public class SecurityConfig {
         http.authorizeRequests(authorizeRequests ->
                                 authorizeRequests
                                         .requestMatchers("/myCards").authenticated()
+                                        .requestMatchers("/auth/login").permitAll()
                                         .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
                                         .requestMatchers(HttpMethod.GET, "/api/users/").authenticated()
                                         .requestMatchers(HttpMethod.DELETE, "/api/users").authenticated()
@@ -32,8 +35,16 @@ public class SecurityConfig {
                                         .requestMatchers("/contact").permitAll()
                                         .anyRequest().authenticated()
                 )
-                .csrf(cr -> cr.disable())
-                .httpBasic(withDefaults());
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/auth/login")
+                        .defaultSuccessUrl("/home")
+                        .failureUrl("/auth/login?error=true")
+                        .permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+//                .csrf(cr -> cr.disable())
+                .formLogin(withDefaults());
+//                .httpBasic(withDefaults());
+        http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
